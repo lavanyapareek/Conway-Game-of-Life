2a55fbb540d2c33694f938157787b543a2706511
lavanyapareek <lavanyapareek@gmail.com>
2023-01-12 21:20:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,255 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useCallback, useRef } from "react";
+import produce from "immer";
+import {information as information} from './information';
+const numRows = 100;
+const numCols = 100;
+const glider = [
+  [0, 1, 0],
+  [0, 0, 1],
+  [1, 1, 1]
+]
+const blinker = [
+  [0, 1, 0],
+  [0, 1, 0],
+  [0, 1, 0]
+]
+const beacon = [
+  [1, 1, 0, 0],
+  [1, 1, 0, 0],
+  [0, 0, 1, 1],
+  [0, 0, 1, 1]
+]
+const toad = [
+  [0, 1, 1, 1],
+  [1, 1, 1, 0],
+]
+const pulsar = [
+  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+]
+const acorn = [
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 0, 0, 1, 1, 1],
+]
+const gosperGliderGun =
+  [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]
+const rpentomino = [[0, 1, 1],
+[1, 1, 0],
+[0, 1, 0]]
+const queenBeeSpaceShip = [[0, 1, 1, 0, 0, 0, 0, 0],
+[1, 0, 0, 1, 0, 0, 0, 0],
+[0, 1, 0, 1, 0, 0, 0, 0],
+[0, 0, 1, 1, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 1, 0, 0],
+[0, 0, 0, 0, 1, 1, 0, 0],
+[0, 0, 0, 0, 0, 0, 1, 1],
+[0, 0, 0, 0, 0, 0, 1, 1]]
 
-function App() {
+const blockLayingSwitchEngine = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]
+]
+
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+
+  return rows;
+};
+export default function App() {
+
+  const[inf, setInf] = useState(information[0]);
+
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid();
+  });
+  const handleGeneration = (pattern, i) => {
+    const newGrid = produce(grid, gridCopy => {
+      for (let i = 0; i < pattern.length; i++) {
+        for (let j = 0; j < pattern[i].length; j++) {
+          gridCopy[i + numCols / 2][j + numCols / 2] = pattern[i][j];
+        }
+      }
+    });
+    setGrid(newGrid);
+    setInf(information[i]);
+  }
+  const generateRandomGrid = () => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => Math.random() < 0.75 ? 0 : 1))
+    }
+    return rows;
+  }
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid((g) => {
+      const gridCopy = Array.from({ length: numRows }, () => Array(numCols).fill(0));
+
+      for (let i = 0; i < numRows; i++) {
+        for (let k = 0; k < numCols; k++) {
+          let neighbors = 0;
+          operations.forEach(([x, y]) => {
+            const newI = i + x;
+            const newK = k + y;
+            if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+              neighbors += g[newI][newK];
+            }
+          });
+
+          if (neighbors < 2 || neighbors > 3) {
+            gridCopy[i][k] = 0;
+          } else if (g[i][k] === 0 && neighbors === 3) {
+            gridCopy[i][k] = 1;
+          } else {
+            gridCopy[i][k] = g[i][k];
+          }
+        }
+      }
+      return gridCopy;
+    });
+
+
+    setTimeout(runSimulation, 0);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className='game-of-life-grid'>
+      <div className='button-class'>
+        <button onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+        }}>
+          {running ? 'stop' : 'start'}
+        </button>
+        <button onClick={() => {
+          setGrid(generateRandomGrid())
+        }}>
+          random-setup
+        </button>
+        <button onClick={() => {
+          setGrid(generateEmptyGrid())
+        }}>
+          clear
+        </button>
+        <button onClick={() => handleGeneration(glider,0)}>
+          glider
+        </button>
+        <button onClick={() => handleGeneration(rpentomino,1)}>
+        R-pentomino
+        </button>
+        <button onClick={() => handleGeneration(queenBeeSpaceShip,2)}>
+          Queen Bee SpaceShip
+        </button>
+        <button onClick={() => handleGeneration(gosperGliderGun,3)}>
+          Gosper Glider Gun
+        </button>
+        <button onClick={() => handleGeneration(acorn,4)}>
+        acorn
+        </button>
+        <button onClick={() => handleGeneration(pulsar,5)}>
+        pulsar
+        </button>
+        <button onClick={() => handleGeneration(beacon,6)}>
+        beacon
+        </button>
+        <button onClick={() => handleGeneration(blinker,7)}>
+        blinker
+        </button>
+        <button onClick={() => handleGeneration(toad,8)}>
+        toad
+        </button>
+        <button onClick={() => handleGeneration(blockLayingSwitchEngine,9)}>
+        block laying switch engine
+        </button>
+        
+        
+        
+        
+        
+      </div>
 
-export default App;
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 5px)`
+      }}>
+        {grid.map((rows, i) =>
+          rows.map((col, k) =>
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: "5px",
+                height: "5px",
+                backgroundColor: grid[i][k] !== 0 ? 'white' : undefined,
+
+              }}
+            />))}
+      </div>
+    </div>
+    <div className='information-class'>
+    <div className='information'>{inf}</div>
+  </div>
+  </>
+  );
+};
+
+
+
+
---
+++ src/information.js
@@ -0,0 +1,15 @@
+const  gameoflife = <h1>gameoflife</h1>
+const glider = <h1>Woah</h1>
+const rpentomonio = <h1>Woah</h1>
+const queenbeespaceship = <h1>Woah</h1>
+const gosperglidergun = <h1>Woah</h1>
+const acorn = <h1>Woah</h1>
+const pulsar = <h1>Woah</h1>
+const beacon  = <h1>Woah</h1>
+const blinker = <h1>Woah</h1>
+const toad = <h1>Woah</h1>
+const blocklayingswitchengine = <h1>Woah</h1>
+
+export const information = [
+    gameoflife, glider, rpentomonio, queenbeespaceship, gosperglidergun ,acorn ,pulsar ,beacon , blinker, toad, blocklayingswitchengine
+]
